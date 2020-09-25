40cad9ac111e4563f92f948bf4a2a8596aac2905
HP <baryla181@gmail.com>
2020-09-25 19:12:20 +0200

    Add PopUp class.

--- app/src/main/java/com/example/layoutsxml/MainActivity.kt
+++ app/src/main/java/com/example/layoutsxml/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.layoutsxml
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 import com.example.layoutsxml.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -13,8 +15,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
        binding.fbLoginButton.setOnClickListener {
-           println("Hello there, I'm working!")
+           val intent = Intent(this,CodePopUp::class.java)
+           intent.putExtra("popuptitle", "Login")
+           intent.putExtra("popuptext", "Great, you're logged in!")
+           intent.putExtra("popupbtn", "OK")
+           intent.putExtra("darkstatusbar", false)
+           startActivity(intent)
        }
+
+        binding.joinStormButton.setOnClickListener {
+            val intent = Intent(this,CodePopUp::class.java)
+            intent.putExtra("popuptitle", "Joining")
+            intent.putExtra("popuptext", "Great, You have joined the BrainStorm!")
+            intent.putExtra("popupbtn", "OK")
+            intent.putExtra("darkstatusbar", false)
+            startActivity(intent)
+        }
     }
 
 }
