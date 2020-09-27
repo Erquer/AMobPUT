6dbd8cda3f599045952246bc49cbc02c20ea3e30
HP <baryla181@gmail.com>
2020-09-27 17:32:32 +0200

    Add activity_code_pop_up.xml Change some activities

--- app/src/main/java/com/example/layoutsxml/MainActivity.kt
+++ app/src/main/java/com/example/layoutsxml/MainActivity.kt
@@ -3,8 +3,13 @@ package com.example.layoutsxml
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AlertDialog
 import androidx.cardview.widget.CardView
 import com.example.layoutsxml.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_code_pop_up.*
+import kotlinx.android.synthetic.main.activity_code_pop_up.view.*
 import java.text.NumberFormat
 
 /**
@@ -18,21 +23,32 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
        binding.fbLoginButton.setOnClickListener {
-           val intent = Intent(this,CodePopUp::class.java)
-           intent.putExtra("popuptitle", "Login")
-           intent.putExtra("popuptext", "Great, you're logged in!")
-           intent.putExtra("popupbtn", "OK")
-           intent.putExtra("darkstatusbar", false)
-           startActivity(intent)
+           val intent2 = Intent(this, LoggedActivity::class.java)
+           startActivity(intent2)
        }
 
         binding.joinStormButton.setOnClickListener {
-            val intent = Intent(this,CodePopUp::class.java)
-            intent.putExtra("popuptitle", "Joining")
-            intent.putExtra("popuptext", "Great, You have joined the BrainStorm!")
-            intent.putExtra("popupbtn", "OK")
-            intent.putExtra("darkstatusbar", false)
-            startActivity(intent)
+          val mDialogView = LayoutInflater.from(this).inflate(R.layout.activity_code_pop_up, null)
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("Code Form")
+
+            val mAlertDialog = mBuilder.show()
+
+            val array = arrayOf("Brainstorm 1","Brainstorm 2","Brainstorm 3","Brainstorm 4","Brainstorm 5")
+            mAlertDialog.brainstorm_spinner.adapter = ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item, array)
+            mDialogView.pass_code_btn.setOnClickListener {
+                mAlertDialog.dismiss()
+                val code = mDialogView.codeEt.text.toString()
+                when(code){
+                    // check if code match to chosen brainstorm.
+                    "12345" -> startActivity(Intent(this, VotingActicity::class.java))
+                    else -> AlertDialog.Builder(this).setMessage("Wrong Code").setTitle("Code Form").show()
+                }
+            }
+            mDialogView.cancel_code_btn.setOnClickListener {
+                mAlertDialog.dismiss()
+            }
         }
     }
 
